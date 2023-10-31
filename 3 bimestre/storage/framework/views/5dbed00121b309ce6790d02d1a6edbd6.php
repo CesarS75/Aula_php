37a33006cfea7ddbc2a47879613d637a792bc6d5
CesarS75 <106964673+CesarS75@users.noreply.github.com>
2023-10-31 07:50:38 -0300

    laravel
    vendor é MUITO pesado entao ta compactado

---
+++ 3 bimestre/storage/framework/views/5dbed00121b309ce6790d02d1a6edbd6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Produtos</title>
+</head>
+<body>
+    <div class="container">
+        <h3>Página de produtos</h3>
+            <hr>
+        <a href="/">Home</a>
+        <a href="contatos">Contato</a>
+    </div>    
+</body>
+</html><?php /**PATH C:\xampp\htdocs\cesar_c\aula\resources\views/produtos.blade.php ENDPATH**/ ?>
---
+++ 3 bimestre/storage/framework/views/8750070b8892b0ed7ac24d1eb11ce8bc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Contato</title>
+</head>
+<body>
+    <div class="container">
+        <h3>Página de contato</h3>
+            <hr>
+        <a href="/">Home</a>
+        <a href="/produtos">Produtos</a>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\cesar_c\aula\resources\views/contatos.blade.php ENDPATH**/ ?>
